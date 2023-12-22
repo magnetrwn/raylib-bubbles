d64117ef1269325c8243ba321398ae323708c662
magnetrwn <rpolli.dev@tutanota.com>
2023-12-22 20:23:54 +0100

    First commit.

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sh
+# Probably should comment out clearing CMake cache files if you need to compile multiple times.
+
+set -e
+
+cd build
+[ -f bubbles ] && rm bubbles
+cmake ..
+make
+rm ../bubbles_build*.zip 2> /dev/null || true
+rm -r CMakeFiles cmake_install.cmake CMakeCache.txt Makefile 2> /dev/null || true
+
+zip -r "bubbles_build$(date +%s).zip" .
+mv bubbles_build*.zip ../
+mv compile_commands.json ../
+
+./bubbles
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+#include "window.hpp"
+
+int main(void) {
+    printf("Hello, world!\n");
+    return 0;
+}
