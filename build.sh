f6681dc435fbddef5112383b63ab6ad3e1f520ae
magnetrwn <rpolli.dev@tutanota.com>
2024-03-05 19:17:58 +0100

    Fixed adding static folder in builds, now only contents from static are added.

--- build.sh
+++ build.sh
@@ -5,14 +5,17 @@ BUILD_FILENAME="bubbles_build$(date +%s).zip"
 
 mkdir -p build
 rsync -a static/ build/
+
 cd build
 [ -f bubbles ] && rm bubbles
 cmake ..
 make
 zip -dcr9 "$BUILD_FILENAME" bubbles
+mv "$BUILD_FILENAME" ../
 cp compile_commands.json ../
-cd ..
-zip -dcr9 build/"$BUILD_FILENAME" static
-mv build/"$BUILD_FILENAME" .
 
+cd ../static
+zip -dcr9 ../"$BUILD_FILENAME" ./*
+
+cd ..
 LSAN_OPTIONS="verbosity=1:log_threads=1" build/bubbles
